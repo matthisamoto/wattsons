9afc84deb08242de5c3573971eed81d99f52739a
Matt Hisamoto <mhisamoto@bullyentertainment.com>
2013-02-20 11:48:58 -0500

    slides are sized and resized

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 (function($)
 {
+	$_Window = $(window);
+	
+	var resizeAll = function(e)
+	{
+		$('.slide').css('width', $_Window.width() + "px");
+	}
+	
+	$_Window.smartresize( resizeAll );
+	
+	resizeAll(null);
 	
 }(jQuery));
